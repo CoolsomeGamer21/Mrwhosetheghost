0ffee16a4ee2c65c7e9e9fb3e22714efd8dd2f4e
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2022-10-22 21:40:11 -0400

    Create giftshop.py

---
+++ Frontend/giftshop.py
@@ -0,0 +1,35 @@
+<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mrwhosetheghost</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+  <body style="background-color:#000000">
+  <header class="header">
+    <a href="#" class="logo">Mrwhosetheghost</a>
+    <nav class="nav-items">
+      <a href="hh_game.html">Haunted House</a>
+      <a href="">MythBuster</a>
+      <a href="">Superstition</a>
+       <a href="Account.html">My Account</a>
+    </nav>
+  </header>
+
+  <main>
+    <h1 style="color:#fff">Choose anything to purchase!</h1>
+      <p1 style="color:#fff">You have $100,000 left to spend</p1>
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="achievements">
+      <img src="https://media0.giphy.com/media/lPG5SO3DpQFSeXGAJ7/giphy.gif" alt="Sonia" width="150" height="150">
+      <button>Purchase 1 for $100</a></button>
+      <button>Purchase 2 for $300</a></button>
+      <button>Purchase 3 for $500</a></button>
+      </html>
