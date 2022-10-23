2c422e5648a5439ec765f672718189d759bdd538
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2022-10-22 21:40:51 -0400

    Create hauntedhouse.js

---
+++ Frontend/hauntedhouse.js
@@ -0,0 +1,36 @@
+<html>
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mrwhosetheghost</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body style="background-color:#000000">
+  <header class="header">
+    <a href="#" class="logo">Mrwhosetheghost</a>
+    <nav class="nav-items">
+      <a href="hh_game.html">Haunted House</a>
+      <a href="">MythBuster</a>
+      <a href="">Superstition</a>
+       <a href="Account.html">My Account</a>
+      
+    </nav>
+  </header>
+
+    <main>
+    <div class="intro">
+      <img src="https://img.freepik.com/premium-vector/interior-scary-house-with-doors-bloody-footprints-candles-halloween-cartoon-vector-illustration-corridor_273525-36.jpg?w=2000" width="700" height="550" alt="">
+      <button onclick="window.location.href = 'room_1.html';">Enter Room 1</button>
+      <br>
+      <button>Enter Room 2</a></button>
+      <br>
+      <button>Enter Room 3</a></button>
+
+    </div>
+</html>
